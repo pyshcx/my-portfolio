051c6268438795ce0e57ddbacd6b1fa822357c5d
pyshcx <pranay.shah2022@vitstudent.ac.in>
2025-02-25 00:04:58 +0530

    Update portfolio content or styling

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -4,15 +4,31 @@
 import { motion } from 'framer-motion';
 
 const Home = () => (
-  <section id="home" className="min-h-screen bg-custom-100 flex flex-col justify-center items-center text-center">
-  <h1 className="text-5xl font-extrabold text-white mb-4">Hi, I'm Pranay Shah</h1>
-  <p className="text-lg text-custom-300 mb-6">
-    Passionate about AI, ML, and autonomous systems, leveraging expertise in computer vision and path planning.
-  </p>
-  <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded-lg hover:bg-blue-600 transition">
-    View My Work
-  </a>
+  <section
+  id="home"
+  className="relative min-h-screen flex flex-col justify-center items-center text-center bg-cover bg-center"
+  style={{
+    backgroundImage: "url('/lidar-img-short-bg.jpg')",
+  }}
+>
+  {/* Overlay */}
+  <div className="absolute inset-0 bg-black opacity-50"></div>
+
+  {/* Content */}
+  <div className="relative z-10">
+    <h1 className="text-5xl font-extrabold text-white mb-4">Hi, I'm Pranay Shah</h1>
+    <p className="text-lg text-gray-300 mb-6">
+      Passionate about AI, ML, and autonomous systems, leveraging expertise in computer vision and path planning.
+    </p>
+    <a
+      href="#projects"
+      className="bg-blue-500 text-white py-3 px-6 rounded-lg hover:bg-blue-600 transition"
+    >
+      View My Work
+    </a>
+  </div>
 </section>
+
 
 );
 
