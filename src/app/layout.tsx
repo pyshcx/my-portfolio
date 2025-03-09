6409b0590eda8041edf72b3b33fdd63e73527f9b
pyshcx <pranay.shah2022@vitstudent.ac.in>
2025-03-09 23:39:46 +0530

    “favicon”

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import './globals.css';
 import { Poppins } from 'next/font/google';
 
@@ -11,12 +10,14 @@ const poppins = Poppins({
 export const metadata = {
   title: 'Pranay Shah | AI & ML Engineer',
   description: 'Portfolio of Pranay Shah, showcasing expertise in AI, ML, and autonomous systems.',
-  keywords: 'AI, ML, Computer Vision, Autonomous Systems, Portfolio',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={poppins.className} style={{ scrollBehavior: 'smooth' }}>
+      <head>
+        <link type="image/png" sizes="96x96" rel="icon" href="/icons8-p-96.png" />
+      </head>
       <body className="bg-gradient-to-br from-[#1E3D58] via-[#3A6EA5] to-[#00BFA6]">
         {children}
       </body>
